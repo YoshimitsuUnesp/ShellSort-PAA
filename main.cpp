fe3ab3aeb618c1fd226da8f31d3b151d4d12cb65
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2022-11-23 11:26:50 -0300

    gap implementado

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cmath>
 
-using namespace std;
+int main()
+{
+    int n;
+    long res = 0;
+    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int main(){
-    cout << "Hello world!" << endl;
+    std::cout << "O gap A036562 é definido por a(n) = 4^(n+1) + 3*2^n + 1" << std::endl;
+    std::cout << "Digite o valor de n" << std::endl;
+    std::cin >> n;
+
+    // Calcula o gap
+    for (int i = 0; i < n; i++)
+    {
+        res = pow(4, i + 1) + 3 * pow(2, i) + 1;
+        std::cout << res << std::endl;
+    }
+
     return 0;
 }
